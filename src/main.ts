52c5ba14567c202e4a0685c1faf4a7774e255250
davepm <davidmendoza911119@gmail.com>
2025-03-02 22:06:00 -0600

    add log in main client-gateway

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envs } from './config';
 import { CustomRpcExceptionFilter } from '@common/exceptions';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Main');
   app.setGlobalPrefix('api');
   app.useGlobalFilters(new CustomRpcExceptionFilter());
   app.useGlobalPipes(
@@ -15,5 +16,7 @@ async function bootstrap() {
     })
   )
   await app.listen(envs.port);
+
+  logger.log(`App running`)
 }
 bootstrap();
