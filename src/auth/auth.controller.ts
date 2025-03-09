c891e29cf93c7dfcc8aa70fa7d1476c926d5245a
davepm <davidmendoza911119@gmail.com>
2025-03-08 22:47:02 -0600

    feature: complete communication with auth-ms

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,11 @@
-import { Controller, Get, Post, Inject, Body } from '@nestjs/common';
+import { Controller, Get, Post, Inject, Body, UseGuards } from '@nestjs/common';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { firstValueFrom } from 'rxjs';
 import { NATS_SERVICE } from '../config';
 import { LoginUserDto, RegisterUserDto } from './dto';
+import { Token, User } from './decorators';
+import { CurrentUser } from './interfaces';
+import { AuthGuard } from './guards/auth';
 
 @Controller('auth')
 export class AuthController {
@@ -24,12 +27,20 @@ export class AuthController {
   
 
   @Post('login')
-  login(@Body() loginUserDto: LoginUserDto) {
-    return this.client.send({ cmd: 'auth.login.user' }, loginUserDto);
+  async login(@Body() loginUserDto: LoginUserDto) {
+    try {
+      const user = await firstValueFrom(this.client.send({ cmd: 'auth.login.user' }, loginUserDto));
+      return user;
+
+    } catch (error) {
+      throw new RpcException(error);
+    }
   }
 
+  @UseGuards(AuthGuard)
   @Get('verify')
-  verify() {
-    return this.client.send({ cmd: 'auth.verify.token' }, {});
+  verify(@User() user: CurrentUser, @Token() token: string) {
+    // return this.client.send({ cmd: 'auth.verify.token' }, {});
+    return { user, token };
   }
 }
---
+++ src/auth/guards/auth/auth.guard.ts
@@ -0,0 +1,46 @@
+
+import { envs } from '@config/envs';
+import { NATS_SERVICE } from '@config/services';
+import {
+  CanActivate,
+  ExecutionContext,
+  Inject,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { Request } from 'express';
+import { firstValueFrom } from 'rxjs';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+
+  constructor(
+    @Inject(NATS_SERVICE) private readonly client: ClientProxy
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+    try {
+      const { user, token: newToken } = await firstValueFrom(this.client.send({ cmd: 'auth.verify.token' }, token));
+
+      console.log({ user, newToken });
+
+      request['user'] = user;
+      request['token'] = newToken;
+
+    } catch {
+      throw new UnauthorizedException();
+    }
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
